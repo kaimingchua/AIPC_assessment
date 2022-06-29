811259c071f8f45ad15894fe3125497d2fe8f43b
chuakaiming <chua.kaiming1@gmail.com>
2022-06-29 01:28:43 +0000

    adding day01 lab stuffs

--- day01/resources.tf
+++ day01/resources.tf
@@ -23,8 +23,8 @@ resource "docker_container" "dov-bear-container" {
 
 resource "docker_container" "dov-bear-unique" {
   for_each = var.containers
-  name  = each.key
-  image = data.docker_image.dov-bear.id
+  name     = each.key
+  image    = data.docker_image.dov-bear.id
   env = [
     "INSTANCE_NAME=${each.key}",
   ]
@@ -38,6 +38,37 @@ resource "docker_container" "dov-bear-unique" {
 #     filename = docker_container.dov-bear-unique[each.key].name
 #     content = join(", ", [for c in docker_container.dov-bear-container: c.name])
 # }
+
+resource "digitalocean_droplet" "nginx" {
+  name     = "nginx"
+  image    = var.DO_image
+  size     = var.DO_size
+  region   = var.DO_region
+  ssh_keys = [data.digitalocean_ssh_key.aipc-fred-key.id]
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file(var.DO_private_key)
+    host = self.ipv4_address
+  }
+  provisioner "remote-exec" {
+      inline = [
+          "apt-update",
+          "apt-install nginx -y",
+          "systemctl enable nginx",
+          "systemctl start nginx"
+      ]
+  }
+}
+
+resource "local_file" "root_at_nginx" {
+    content = ""
+    filename = "root@${digitalocean_droplet.nginx.ipv4_address}"
+}
+
+output "nginx_ip_address" {
+    value = digitalocean_droplet.nginx.ipv4_address
+}
 
 output "mykey_ssh_key" {
   value = data.digitalocean_ssh_key.aipc-fred-key.fingerprint
@@ -53,5 +84,5 @@ output "dov-bear-MD5" {
 }
 
 output "container-names" {
-    value = [ for c in docker_container.dov-bear-container: c.name ]
+  value = [for c in docker_container.dov-bear-container : c.name]
 }
--- day01/variables.tf
+++ day01/variables.tf
@@ -4,6 +4,27 @@ variable "DO_token" {
   default   = "dop_v1_3ae7fa3a18a67a2886cacb22fc7e7bd99d421296a9cb0057b3cbf6eb12fb4a7d"
 }
 
+variable "DO_private_key" {
+  type      = string
+  sensitive = true
+  default = "./fred.pem"
+}
+
+variable "DO_image" {
+  type    = string
+  default = "ubuntu-20-04-x64"
+}
+
+variable "DO_region" {
+  type    = string
+  default = "sgp1"
+}
+
+variable "DO_size" {
+  type    = string
+  default = "s-1vcpu-1gb"
+}
+
 variable "ports" {
   type    = list(number)
   default = [8080, 8081, 8082, 8083]
@@ -16,10 +37,10 @@ variable "containers" {
 
   default = {
     abc = {
-        external_port : 1234
+      external_port : 1234
     }
     xyz = {
-        external_port : 9090
-    }  
+      external_port : 9090
+    }
   }
 }
